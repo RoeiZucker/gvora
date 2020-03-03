b6ee422fea3defd22389cf425561371c96608491
RoeiZucker <roeissss@gmail.com>
2020-03-03 19:28:10 +0200

    asdf

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/product', productRouter);
 
-const port = 80;
+const port = const PORT = process.env.PORT || 3000;
 
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
